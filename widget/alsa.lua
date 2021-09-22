a89d7cb165c3362fb4206f12eab8cc972726300b
Albin Wahlén <alegow8@gmail.com>
2021-09-22 23:03:24 +0200

    Get connection info (headphone/mic)

--- widget/alsa.lua
+++ widget/alsa.lua
@@ -10,6 +10,22 @@ local helpers = require("lain.helpers")
 local shell   = require("awful.util").shell
 local wibox   = require("wibox")
 local string  = string
+local gears = require("gears")
+
+local function get_connections(mixer)
+    t = { headphones = false, microphone = false}
+    for name, status in string.gmatch(mixer, 
+            "name='([%a%s%-]+)'.-: values=([%a]+)") do
+        status = true and (status == "on") or false
+        -- We are only intrested in headphones and microphone
+        if name == "Headphone Jack" then
+            t.headphones = status
+        elseif name == "Mic Jack" then
+            t.microphone = status
+        end
+    end
+    return t
+end
 
 -- ALSA volume
 -- lain.widget.alsa
@@ -32,18 +48,34 @@ local function factory(args)
     end
 
     alsa.last = {}
+    last_connections = {}
 
     function alsa.update()
-        helpers.async(format_cmd, function(mixer)
-            local l,s = string.match(mixer, "([%d]+)%%.*%[([%l]*)")
-            l = tonumber(l)
-            if alsa.last.level ~= l or alsa.last.status ~= s then
-                volume_now = { level = l, status = s }
-                widget = alsa.widget
-                settings()
-                alsa.last = volume_now
-            end
-        end)
+        helpers.async("amixer -c 1 contents", function(mixer)
+            connections = get_connections(mixer)
+            helpers.async(format_cmd, function(mixer)
+                gears.debug.dump("\nFrom lain")
+                gears.debug.dump("Headphones: " .. tostring(connections.headphones))
+                gears.debug.dump("Microphone: " .. tostring(connections.microphone))
+                local l,s = string.match(mixer, "([%d]+)%%.*%[([%l]*)")
+                gears.debug.dump("Level: " .. l)
+                gears.debug.dump("Status: " .. s)
+                l = tonumber(l)
+                if alsa.last.level ~= l or alsa.last.status ~= s
+                    or last_connections.headphones ~= connections.headphones 
+                    or last_connnections.microphone ~= connectins.microphone then
+                    gears.debug.dump(tostring(alsa.last.level ~= l) .. 
+                        " or " .. tostring(alsa.last.status ~= s) ..
+                        " or " .. tostring(last_connections.headphones ~= connections.headphones) ..
+                        " or " .. tostring(last_connections.microphone ~= connections.microphone))
+                    volume_now = { level = l, status = s }
+                    widget = alsa.widget
+                    settings()
+                    alsa.last = volume_now
+                    last_connections = connections
+                end
+            end)
+            end)
     end
 
     helpers.newtimer(string.format("alsa-%s-%s", alsa.cmd, alsa.channel), timeout, alsa.update)
